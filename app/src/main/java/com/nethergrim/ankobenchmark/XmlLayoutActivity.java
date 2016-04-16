0fffdddfc5c76b723ca9be6023cb74af28c45fd7
Andrey Drobyazko <c2q9450@gmail.com>
2016-04-16 22:58:47 +0300

    Created project

---
+++ app/src/main/java/com/nethergrim/ankobenchmark/XmlLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.nethergrim.ankobenchmark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XmlLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xml_layout);
+    }
+}
